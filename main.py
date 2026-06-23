c234b4d94311055f0ab1136030c56b1298c0a237
blokz <thebloxpl@gmail.com>
2026-06-23 19:03:39 +0200

    added a basic techtreegraph

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
 import dotenv
 import os
+import data
 
 dotenv.load_dotenv()
 
@@ -13,11 +14,25 @@ if not all([uri, user, password]):
 
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
+df = data.preprocess_data()
+def create_techs(tx):
+    for _, row in df.iterrows():
+        tech = row['Technology']
+        targets = row['Leads to']
+        for t in targets:
+            tx.run("""
+                MERGE (a:Technology {name: $tech})
+                MERGE (b:Technology {name: $target})
+                MERGE (a)-[:LEADS_TO]->(b)
+            """, tech=tech, target=t)
+        
+
 def setup(tx):
     tx.run("MATCH (n) DETACH DELETE n")
     print("Dane załadowane!")
 
 with driver.session() as session:
     session.execute_write(setup)
+    session.execute_write(create_techs)
 
 driver.close()
